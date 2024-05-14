0521f077f7b284bcb196fa2aeb42cda212763184
amirb <amirbou@gmail.com>
2024-05-14 11:29:26 +0300

    Add the planned libraries

---
+++ src/capture.rs
@@ -0,0 +1,6 @@
+//! This module captures binder transactions
+//! 
+//! TODO create different "backends" to handle the capture (ptrace, uprobe, tracepoints etc.)
+
+
+pub mod tracepoints;
---
+++ src/pcapng.rs
@@ -0,0 +1,2 @@
+//! This module creates a valid pcapng file from binder data
+//! captured by the `capture` module and parsed by the `binder` module
