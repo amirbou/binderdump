d466190e76cd523680fccaf9cc7d8d7e24ab7f3e
amirb <amirbou@gmail.com>
2024-05-20 00:34:00 +0300

    example works on Android too!

--- src/bin.rs
+++ src/bin.rs
@@ -1,3 +1,4 @@
+use std::os::unix::fs::MetadataExt;
 use libbpf_rs::skel::{OpenSkel, Skel, SkelBuilder};
 use log::debug;
 use libc;
@@ -21,6 +22,20 @@ fn bump_memlock_rlimit() -> Result<()> {
     Ok(())
 }
 
+fn get_pid_ns() -> Result<(u64, u64)> {
+    let metadata = std::fs::metadata("/proc/self/ns/pid")?;
+    let dev = metadata.dev();
+    let ino = metadata.ino();
+
+    Ok((dev, ino))
+}
+
+// On android tracing is off by default
+fn enable_bpf_printk_trace() -> Result<()> {
+    std::fs::write("/sys/kernel/tracing/tracing_on", "1\n")?;
+    Ok(())
+}
+
 pub fn main() -> Result<()> {
     #[cfg(target_os = "android")]
     android_logger::init_once(
@@ -33,9 +48,12 @@ pub fn main() -> Result<()> {
     
     debug!("Hello");
     let pid = std::process::id();
-    println!("mypid: {} {} {}", pid, pid as i32, unsafe { libc::getpid()});
+
+
+    // println!("mypid: {} {} {}", pid, pid as i32, unsafe { libc::getpid()});
 
     bump_memlock_rlimit()?;
+    enable_bpf_printk_trace()?;
 
     let mut skel_builder = MinimalSkelBuilder::default();
     skel_builder.obj_builder.debug(true);
@@ -43,16 +61,24 @@ pub fn main() -> Result<()> {
     let mut open_skel = skel_builder.open()?;
 
     open_skel.rodata_mut().my_pid = pid as i32;
-
+    
+    if let Ok((dev, ino)) = get_pid_ns() {
+        open_skel.rodata_mut().my_dev = dev;
+        open_skel.rodata_mut().my_ino = ino;
+        open_skel.rodata_mut().check_ns = 1;
+    } else {
+        open_skel.rodata_mut().check_ns = 0;
+    }
+    
     let mut skel = open_skel.load()?;
     skel.attach()?;
 
     println!("mypid: {}", pid);
 
-    loop {
+    for _ in 0..10 {
         println!("Hi");
         unsafe {libc::syscall(libc::SYS_io_destroy, 0) };
-        std::thread::sleep(std::time::Duration::from_secs(5));
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
     Ok(())
 }
