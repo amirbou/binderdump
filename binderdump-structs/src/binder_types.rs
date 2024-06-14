1c3def3e506b16b7aa84982d08e4fdf19a0d8176
amirb <amirbou@gmail.com>
2024-06-14 21:47:49 +0300

    force binder_ioctl enum to use the same type for all build targets

--- binderdump-structs/src/binder_types.rs
+++ binderdump-structs/src/binder_types.rs
@@ -73,35 +73,42 @@ pub enum binder_buffer_flag {
     HAS_PARENT = binderdump_sys::BINDER_BUFFER_FLAG_HAS_PARENT,
 }
 
+// nix request_code_readwrite! macro uses ioctl_num_type, which is defined as `int` when targeting Android or musl,
+// and `long` otherwise. We always want `int`
+macro_rules! request_code_readwrite_wrapper {
+    ($nr:expr, $type_:ty) => {
+        request_code_readwrite!('b', $nr, size_of::<$type_>()) as i32
+    };
+}
+
+macro_rules! request_code_write_wrapper {
+    ($nr:expr, $type_:ty) => {
+        request_code_write!('b', $nr, size_of::<$type_>()) as i32
+    };
+}
+
 #[derive(Debug, FromPrimitive, Clone, Copy, PartialEq, Eq, Default)]
 #[allow(non_camel_case_types)]
 #[repr(i32)]
 #[binrw]
 #[brw(repr(i32))]
 pub enum binder_ioctl {
-    BINDER_WRITE_READ =
-        request_code_readwrite!('b', 1, size_of::<binderdump_sys::binder_write_read>()),
-    BINDER_SET_IDLE_TIMEOUT = request_code_write!('b', 3, size_of::<i64>()),
-    BINDER_SET_MAX_THREADS = request_code_write!('b', 5, size_of::<u32>()),
-    BINDER_SET_IDLE_PRIORITY = request_code_write!('b', 6, size_of::<i32>()),
-    BINDER_SET_CONTEXT_MGR = request_code_write!('b', 7, size_of::<i32>()),
-    BINDER_THREAD_EXIT = request_code_write!('b', 8, size_of::<i32>()),
+    BINDER_WRITE_READ = request_code_readwrite_wrapper!(1, binderdump_sys::binder_write_read),
+    BINDER_SET_IDLE_TIMEOUT = request_code_write_wrapper!(3, i64),
+    BINDER_SET_MAX_THREADS = request_code_write_wrapper!(5, u32),
+    BINDER_SET_IDLE_PRIORITY = request_code_write_wrapper!(6, i32),
+    BINDER_SET_CONTEXT_MGR = request_code_write_wrapper!(7, i32),
+    BINDER_THREAD_EXIT = request_code_write_wrapper!(8, i32),
     #[default]
-    BINDER_VERSION = request_code_readwrite!('b', 9, size_of::<binderdump_sys::binder_version>()),
+    BINDER_VERSION = request_code_readwrite_wrapper!(9, binderdump_sys::binder_version),
     BINDER_GET_NODE_DEBUG_INFO =
-        request_code_readwrite!('b', 11, size_of::<binderdump_sys::binder_node_debug_info>()),
-    BINDER_GET_NODE_INFO_FOR_REF = request_code_readwrite!(
-        'b',
-        12,
-        size_of::<binderdump_sys::binder_node_info_for_ref>()
-    ),
+        request_code_readwrite_wrapper!(11, binderdump_sys::binder_node_debug_info),
+    BINDER_GET_NODE_INFO_FOR_REF =
+        request_code_readwrite_wrapper!(12, binderdump_sys::binder_node_info_for_ref),
     BINDER_SET_CONTEXT_MGR_EXT =
-        request_code_write!('b', 13, size_of::<binderdump_sys::flat_binder_object>()),
-    BINDER_FREEZE = request_code_write!('b', 14, size_of::<binderdump_sys::binder_freeze_info>()),
-    BINDER_GET_FROZEN_INFO = request_code_readwrite!(
-        'b',
-        15,
-        size_of::<binderdump_sys::binder_frozen_status_info>()
-    ),
-    BINDER_ENABLE_ONEWAY_SPAM_DETECTION = request_code_write!('b', 16, size_of::<u32>()),
+        request_code_write_wrapper!(13, binderdump_sys::flat_binder_object),
+    BINDER_FREEZE = request_code_write_wrapper!(14, binderdump_sys::binder_freeze_info),
+    BINDER_GET_FROZEN_INFO =
+        request_code_readwrite_wrapper!(15, binderdump_sys::binder_frozen_status_info),
+    BINDER_ENABLE_ONEWAY_SPAM_DETECTION = request_code_write_wrapper!(16, u32),
 }
